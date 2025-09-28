9f3b63824f3063e7c7fe8f0d568920f9d5345282
Aleem <ikealeem18@gmail.com>
2025-09-28 10:29:12 +0100

    played with if statements a bit

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,11 @@
 ﻿int firstValue = 500;
 int secondValue = 600;
 int largervalue = Math.Max(firstValue, secondValue);
+int smallervalue = Math.Min(firstValue, secondValue);
 
 Console.WriteLine(largervalue);
+Console.WriteLine(smallervalue);
+
+if (largervalue > smallervalue) {
+    Console.WriteLine("Yup, this is accurate");
+}
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+068b09b5f13bcc8d284379ae20f3e82102e5fe0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77648e91aff992f5f1a29acfe2fe08b3f700e910")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
