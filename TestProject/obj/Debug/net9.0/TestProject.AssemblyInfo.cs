e59e3acfca98d5198148205a20c738c808405b82
Aleem <ikealeem18@gmail.com>
2025-09-29 09:55:31 +0100

    Dice game, but better structured

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3b63824f3063e7c7fe8f0d568920f9d5345282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e50f8f0d105515b93ad354b669c0b0226a6b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
