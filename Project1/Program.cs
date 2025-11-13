c11e5c1310df847b44603d8fa5efd3931c83f4d6
Aleem <ikealeem18@gmail.com>
2025-11-13 12:43:48 +0100

    What's this?

---
+++ Project1/Program.cs
@@ -0,0 +1,19 @@
+﻿Random number = new Random();
+int correctGuess = number.Next(0, 100);
+
+string[] guesses = [];
+
+Console.Write("Enter your first guess: ");
+string firstGuess = Console.ReadLine();
+int guess1 = int.Parse(firstGuess);
+
+if (guess1 == correctGuess) {
+    Console.WriteLine("You win!");
+} else if (guess1 > correctGuess) {
+    Console.WriteLine("Your guess is too high!");
+} else if (guess1 < correctGuess) {
+    Console.WriteLine("Your guess is too low!");
+}
+
+Console.WriteLine(correctGuess);
+Console.WriteLine(guess1);
