b980ae955836474ee45ee7d4d090e36b0eb962de
Aleem <ikealeem18@gmail.com>
2025-11-19 18:13:04 +0100

    My First Text-based Role Player Game ✨

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,20 +1,57 @@
 ﻿Random random = new Random();
-int current = random.Next(1, 11);
+int damage;
+
+int heroHealth = 10;
+int monsterHealth = 10;
+int turn = 0;
+do
+{
+    damage = random.Next(1, 11);
+    turn += 1;
+    if (turn % 2 == 1)
+    {
+        monsterHealth -= damage;
+
+        Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health");
+    } else if (turn % 2 == 0)
+    {
+        heroHealth -= damage;
+
+        Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health");
+    }
+
+     
+}   while ( heroHealth > 0 && monsterHealth > 0);
+
+if (heroHealth <= 0)
+    {
+        Console.WriteLine("Monster wins!");
+    } else if (monsterHealth <= 0)
+    {
+        Console.WriteLine("Hero wins!");
+    }
+
+    /*
+    Similar code
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
 
 do
 {
-    current = random.Next(1, 11);
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
 
-    if (current >= 8) continue;
-    
-    Console.WriteLine(current);
-} while (current != 7);
+    if (monster <= 0) continue;
 
-/*
-while (current >= 3)
-{
-    Console.WriteLine(current);
-    current = random.Next(1, 11);
-}
-Console.WriteLine($"Last number: {current}");
-*/
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
+    */
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c90b6b0e89f27944d8a0ea5227ffbcc43f42ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63adcec599f0ddf474cf0e245eb867d0a8e09ca3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
