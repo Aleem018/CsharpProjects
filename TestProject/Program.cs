6cd35b991b48384ca49750e63b5cff4e87ff7e1a
Aleem <ikealeem18@gmail.com>
2025-11-29 22:05:11 +0100

    Exercise on determining how to output math operations as specific number types

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,17 +1,16 @@
-﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
-float total = 0;
+﻿int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
 
-string message = "";
-foreach (var value in values)
-{
-    float number;
-    if (float.TryParse(value, out number))
-    {
-        total += number;
-    } else
-    {
-        message += value;
-    }
-}
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Your code here to set result1
+int result1 = Convert.ToInt32(value1 / value2);
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = (decimal) ( value2 / (decimal)value3);
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = (float) (value3 / (float)value1);
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
