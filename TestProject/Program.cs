2176725fd93d19e33da867b35c37cebc261c1b39
Aleem <ikealeem18@gmail.com>
2025-11-29 21:11:00 +0100

    converting datatypes using casting and conversion techniques

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,22 +1,39 @@
-﻿Console.WriteLine("Signed integral types: ");
+﻿// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
 
-Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
 
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
 
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
 
-int[] data = new int[3];
+// decimal myDecimal = 1.23456789m;
+// float myFloat = (float)myDecimal;
+
+// Console.WriteLine($"Decimal: {myDecimal}");
+// Console.WriteLine($"Float  : {myFloat}");
+
+// string first = "5";
+// string second = "7";
+// int sum = int.Parse(first) + int.Parse(second);
+// Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+int value = (int)1.5m;
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m);
+Console.WriteLine(value2);
+
