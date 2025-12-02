2d583675d9f9359de5e3d37be20dc4f4322b5f2a
Aleem <ikealeem18@gmail.com>
2025-12-02 23:31:31 +0100

    Challenge to reverse the words in a sentence

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,69 +1,17 @@
-﻿// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
 
-// Console.WriteLine("Sorted...");
-// Array.Sort(pallets);
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
+string[] words = pangram.Split(' ');
+string[] reversedArray = new string[words.Length];
 
-// Console.WriteLine("");
-// Console.WriteLine("Reversed...");
-// Array.Reverse(pallets);
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-
-
-
-// Console.WriteLine("");
-
-// Array.Clear(pallets, 0, 2);
-
-// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-
-// Console.WriteLine("");
-// Array.Resize(ref pallets, 6);
-// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-// pallets[4] = "C01";
-// pallets[5] = "C02";
-
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-
-// Console.WriteLine("");
-// Array.Resize(ref pallets, 3);
-// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
-
-// foreach (var pallet in pallets)
-// {
-//     Console.WriteLine($"-- {pallet}");
-// }
-
-// To reverse an array
-// string value = "abc123";
-// char[] valueArray = value.ToCharArray();
-// Array.Reverse(valueArray);
-// string result = new string(valueArray);
-// Console.WriteLine(result);
-
-// To separate it's characters using a comma
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-string result = String.Join(",", valueArray);
-Console.WriteLine(result);
-
-string[] items = result.Split(',');
-foreach (string item in items)
+for (int i = 0; i < words.Length; i++)
 {
-    Console.WriteLine(item);
+    char[] charArray = words[i].ToCharArray();
+    Array.Reverse(charArray);
+    string result = new string(charArray);
+    reversedArray[i] = result;
 }
+
+string final = String.Join(" ", reversedArray);
+Console.WriteLine(final);
+
+
