aac93103998d1950525ba4bad2965c44ef622f76
Aleem <ikealeem18@gmail.com>
2025-12-08 21:48:53 +0100

    testing Remove() and Replace()

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,59 +1,8 @@
-﻿// string message = "Hello, world!";
-// char[] charsToFind = { 'a', 'e', 'i' };
+﻿string data = "12345John Smith          5000  3  ";
+string updatedData = data.Remove(5, 20);
+Console.WriteLine(updatedData);
 
-// int index = message.IndexOfAny(charsToFind);
-
-// Console.WriteLine($"Found '{message[index]}' in '{message}' at index: {index}.");
-
-// string message = "Help (find) the {opening symbols}";
-// Console.WriteLine($"Searching THIS Mesage: {message}");
-// char[] openSymbols = { '[', '{', '(' };
-// int startPosition = 5;
-// int openingPosition = message.IndexOfAny(openSymbols);
-// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
-
-// openingPosition = message.IndexOfAny(openSymbols, startPosition);
-// Console.WriteLine($"Found WITH using startPosition {startPosition}: {message.Substring(openingPosition)}");
-
-string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
-
-// The IndexOfAny() method requires a char array of characters
-
-char[] openSymbols = { '[', '{', '(' };
-
-int closingPosition = 0;
-
-while (true)
-{
-    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
-
-    if (openingPosition == -1) break;
-
-    string currentSymbol = message.Substring(openingPosition, 1);
-
-    // Now, to find the matching closing symbol
-    char matchingSymbol = ' ';
-
-    switch (currentSymbol)
-    {
-        case "[":
-            matchingSymbol = ']';
-            break;
-        case "{":
-            matchingSymbol = '}';
-            break;
-        case "(":
-            matchingSymbol = ')';
-            break;
-    }
-
-    // To find the closingPosition, use an overload of the IndexOf method to specify that the search for the matching symbol should start at the openingPosition in the string
-
-    openingPosition += 1;
-    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
-
-    // Finally...
-
-    int length = closingPosition - openingPosition;
-    Console.WriteLine(message.Substring(openingPosition, length));
-}
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+Console.WriteLine(message);
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+900c8f84b43c1ba8fedcbc10c0be4a2d25e9bef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d21ea0ad2ce7e4f0408726316ac947b4b97688")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
