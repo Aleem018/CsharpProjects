bbadeb10c089bc9de94604030e0bfc5512fef6b2
Aleem <ikealeem18@gmail.com>
2025-12-13 23:13:48 +0100

    Creating reusable methods using the DRY principle!

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9f29545e3cd12a4a877957f1dba9bc8da3be41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2014ba27ace3a49109aae036f39b2f3666ef98")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
