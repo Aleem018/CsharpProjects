e8b28e1b4e38e667b8d5805927bba66dda52bd9d
Aleem <ikealeem18@gmail.com>
2025-12-15 22:51:38 +0100

    ??

--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff91d535894934b439a24ab9142a03fbaa71eb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4445f6e44bff2dde3915f67168000078bfee828")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
