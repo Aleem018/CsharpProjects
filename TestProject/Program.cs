ff5ca111a10c07d19e5ab77204d86df811d23496
Aleem <ikealeem18@gmail.com>
2025-12-24 22:38:59 +0100

    reversing words and sentences

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,157 +1,26 @@
-﻿string ?readResult = "";
-bool reDo= false;
+﻿string input = "there are snakes at the zoo";
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
 
-do
+string ReverseWord(string word)
 {
-    Console.WriteLine("Welcome to Aleem's Exchange System!");
-    Console.WriteLine("Please select the currencies you want to exchange");
-    Console.WriteLine("1. USD to NGN");
-    Console.WriteLine("2. NGN to USD");
-    Console.WriteLine("3. USD to GBP");
-    Console.WriteLine("4. GBP to USD");
-    Console.WriteLine("5. GBP to NGN");
-    Console.WriteLine("6. NGN to GBP");
-    Console.WriteLine();
-
-    int selection = 0;
-    bool errorBoolean = true;
-    do
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--)
     {
-        Console.Write("Select your option: ");
-
-        readResult = Console.ReadLine();
-    
-        if (readResult != null)
-        {
-            errorBoolean = int.TryParse(readResult, out selection);
-            if (errorBoolean == false)
-            {
-                Console.WriteLine("Enter a valid option!");
-            }
-        }
-        else
-        {
-            Console.WriteLine("select a valid option!");
-        }
-    } while (!errorBoolean);
-    
-        switch (selection)
-        {
-            case 1:
-                double USD = 0;
-                do
-                {
-                    Console.Write("Enter your USD amount 💵: ");
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        errorBoolean = double.TryParse(readResult, out USD);
-                    } else
-                    {
-                        errorBoolean = false;
-                        Console.WriteLine("Invalid Input");
-                        continue;
-                    }
-
-                    if (errorBoolean)
-                        {
-                            Console.WriteLine($"${USD} = {USDToNGN(USD)} NGN");
-                            
-                        } else
-                        {
-                            Console.WriteLine("Invalid Input");
-                        }
-                } while(!errorBoolean);
-
-                break;
-
-
-            case 2:
-                double NGN = 0;
-                do
-                {   
-                    Console.Write("Enter your NGN amount 💵: ");
-                    readResult = Console.ReadLine();
-                    if (readResult != null)
-                    {
-                        errorBoolean = double.TryParse(readResult, out NGN);
-                    } else
-                    {
-                        continue;
-                    }
-                    if (errorBoolean)
-                    {
-                        Console.WriteLine($"{NGN} NGN = {NGNToUSD(NGN):C}");
-                    } else
-                    {
-                        Console.WriteLine("Invalid amount");
-                        continue;
-                    }
-                } while(!errorBoolean);
-                break;
-
-            case 3:
-                Console.WriteLine("Work in Progress 👷‍♂️, check back later!");
-                break;
-
-            case 4:
-                Console.WriteLine("Work in Progress 👷‍♂️, check back later!");
-                break;
-
-            case 5:
-                Console.WriteLine("Work in Progress 👷‍♂️, check back later!");
-                break;
-
-            case 6:
-                Console.WriteLine("Work in Progress 👷‍♂️, check back later!");
-                break;
-
-            default:
-                Console.WriteLine("Select between options 1 - 6!");
-                break;
-        }
-
-    Console.WriteLine("Do you want to exchange again? (y/n)");
-    string choice = "";
-
-    do
-    {
-        
-    readResult = Console.ReadLine();
-    if (readResult != null)
-    {
-        choice = readResult.ToLower().Trim();
+        result += word[i];
     }
-    else
-    {
-        continue;
-    }
-    if (choice == "y")
-    {
-        reDo = true;
-        errorBoolean = false;
-    } else if (choice == "n")
-    {
-        reDo = false;
-        errorBoolean = false;
-    } else
-    {
-        Console.WriteLine("Enter a valid output!");
-    }
-    } while(errorBoolean);
-
-    
-
-} while(reDo);
-
-
-double USDToNGN(double USD)
-{
-    double rate = 1460.23;
-    return (rate * USD);
+    return result;
 }
-double NGNToUSD(double NGN)
+
+string ReverseSentence(string input)
 {
-    double rate = 1460.23;
-    return (NGN / rate);
+    string result = "";
+    string[] words = input.Split(" ");
+
+    foreach(string word in words)
+    {
+        result += ReverseWord(word) + " ";
+    }
+
+    return result.Trim();
 }
--- TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c29cf53eb6e22af84cfef0e19ebf1ebdd28b09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b085db2056a0a0b082dd90bc74d5fac114f7c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
